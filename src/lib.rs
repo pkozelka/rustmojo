2249248602e4a806046bdc428843929f3d04a6da
Petr Kozelka <pkozelka@gmail.com>
2018-09-17 11:11:35 +0200

    added some structs
    Signed-off-by: Petr Kozelka <pkozelka@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,62 @@
+use std::collections::HashMap;
+
+struct DomainInfo {
+    num_levels: u32, // TODO: is this same as levels.size??
+    filename: String,
+    levels: Vec<String>,
+}
+
+struct MojoModelInfo {
+    properties: HashMap<String, String>,
+    columns: Vec<String>,
+    domains: HashMap<i32, DomainInfo>,
+    domain_lengths: Vec<i32>,
+}
+
+enum ModelCategory {
+    Unknown      = 0,
+    Binomial     = 1,
+    Multinomial  = 2,
+    Regression   = 3,
+    Clustering   = 4,
+    AutoEncoder  = 5,
+    DimReduction = 6
+}
+
+struct MojoModel {
+    mojo_version_major: i32,
+    mojo_version_minor: i32,
+    info: MojoModelInfo,
+    category: ModelCategory,
+    uuid: String,
+    supervised: bool,
+    nfeatures: i32,
+    nclasses: i32,
+    balance_classes: i32,
+    default_threshold: i32,
+    prior_class_distrib: Vec<f32>,
+    model_class_distrib: Vec<f32>,
+    empty_vector_of_strings: Vec<String>,
+}
+
+impl MojoModel {
+    fn load(filename: &str) -> Result<MojoModel, &str> {
+        panic!("not implemented yet");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    use MojoModel;
+
+    #[test]
+    fn can_load() {
+        let mm = MojoModel::load("test.mdl")?;
+//        let ModelCategory c = ModelCategory::Binomial();
     }
 }
