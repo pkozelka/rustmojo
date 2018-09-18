e7ee8f979cb2e2d704e0214a3a35dd760affa8b5
Petr Kozelka <petr.kozelka@teradata.com>
2018-09-18 03:50:15 +0200

    more playing

--- examples/ex2.rs
+++ examples/ex2.rs
@@ -1,4 +1,10 @@
 
 fn main() {
     println!("Example2");
+    let mut a: Vec<i32> = Vec::new();
+    for n in 1..=5 {
+        println!("n={}", n);
+        a.push(n);
+    }
+    println!("a3={}",a[3]);
 }
