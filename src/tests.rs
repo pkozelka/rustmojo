cbd785104e54961670b66dad26f58d287cffcc8e
Petr Kozelka <pkozelka@gmail.com>
2018-11-19 23:19:08 +0100

    added tests for the future functionality

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,35 @@
+use mojo::Mojo;
+use std::collections::HashMap;
+
 #[test]
 fn it_works() {
     assert_eq!(2 + 2, 4);
 }
 
+#[test]
+#[ignore]
+fn test_binomial_easy() {
+    let mojo = Mojo::load("data/gbm_v1.00_names.mojo").unwrap();
+    let row: HashMap<&str,&str> = [
+        ("AGE", "68"),
+        ("RACE", "2"),
+        ("DCAPS", "2"),
+    ].iter().cloned().collect();
+
+    let prediction = mojo.predict_binomial_easy(row).unwrap();
+    assert_eq!(0.5, prediction.p0);
+    assert_eq!(0.5, prediction.p1);
+    assert_eq!("EASY_DUMMY", prediction.label);
+
+}
+
+#[test]
+fn test_binomial_raw() {
+    let mojo = Mojo::load("data/gbm_v1.00_names.mojo").unwrap();
+    let row = vec![68f64, 2f64, 2f64];
+
+    let prediction = mojo.predict_binomial(row).unwrap();
+    assert_eq!(0.5, prediction.p0);
+    assert_eq!(0.5, prediction.p1);
+    assert_eq!("RAW_DUMMY", prediction.label);
+}
