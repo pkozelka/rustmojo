636322f0b3ae12a618eb3e8766c71d829a9464bc
Petr Kozelka <pkozelka@gmail.com>
2018-12-30 15:31:38 +0100

    printtree now uses acqua model too

--- examples/printtree.rs
+++ examples/printtree.rs
@@ -1,38 +1,39 @@
 extern crate rustmojo as this;
 
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+use this::acqua::acquamodel::Comparison;
+use this::acqua::acquamodel::Node;
 use this::mojoreader::MojoInformation;
 use this::mojoreader::MojoReader;
-use this::mojoreader::SplitValue;
-use this::mojoreader::SubNode;
-use std::fs::File;
-use std::io::Read;
-use std::io;
 
-fn treeprint(indent: &str, node: &SubNode) {
+fn treeprint(indent: &str, node: &Node) {
     match node {
-        SubNode::Leaf(value) => {
+        Node::ValueNode(value) => {
             println!("{}   {}", indent, value)
         },
-        SubNode::NestedNode(split) => {
-            let condition = match split.split_value {
-                SplitValue::IsNotANumber => {
-                    String::from("is NaN ?")
+        Node::DecisionNode(decision) => {
+            let comparison = match decision.condition.comparison {
+                Comparison::None => {
+                    String::from("true")
                 },
-                SplitValue::IsLessOrEqualTo(f) => {
-                    format!("is <= {}", f)
+                Comparison::IsLessThan(f) => {
+                    format!("is < {}", f)
                 },
-                SplitValue::IsPresentInSet(_) => {
+                Comparison::BitsetContains(_) => {
                     String::from("is in set(...todo...)")
                 },
             };
-            println!("{}   split: Col{} is {}", indent, split.split_column_id, condition);
+            println!("{}   if Col{} {}", indent, decision.column.get_column_no(), comparison);
             let mut indent = String::from(indent.clone());
             indent.push(' ');
             indent.push(' ');
-            println!("{} left:", indent);
-            treeprint(&indent, &split.left);
-            println!("{} right:", indent);
-            treeprint(&indent, &split.right);
+            println!("{} then", indent);
+            treeprint(&indent, &decision.do_then);
+            println!("{} else", indent);
+            treeprint(&indent, &decision.do_else);
         },
     }
 }
@@ -43,10 +44,6 @@ fn main() {
     let size = file.metadata().unwrap().len();
     println!("file size is {}", size);
     let root = read_file(&mut file).expect("ERROR");
-    match root {
-        SubNode::Leaf(value) => println!("leaf value is {}", value),
-        SubNode::NestedNode(_) => println!("subnode")
-    }
     println!("byte count is {}", size);
 
     treeprint("", &root);
@@ -60,9 +57,9 @@ fn main() {
 */
 }
 
-fn read_file(file: &mut File) -> io::Result<SubNode> {
+fn read_file(file: &mut File) -> io::Result<Node> {
     let mut buf = Vec::new();
-    let bytes = file.read_to_end(&mut buf)?;
+    file.read_to_end(&mut buf)?;
     let mut reader = MojoReader::new(MojoInformation::new());
-    reader.read_node(&mut buf.iter())
+    reader.read_tree(&mut buf.iter())
 }
